3bbfc768b30d6cad22dc76456f61c52c1de3e683
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-12-03 22:27:07 +0200

    final push, this looks doable

--- src/ui/scenario_controls.cpp
+++ src/ui/scenario_controls.cpp
@@ -3,14 +3,24 @@
 
 ScenarioControls::ScenarioControls() {
     QVBoxLayout *controls_layout = new QVBoxLayout;
-    QPushButton *scenario_file_button = new QPushButton{"Choose a scenario file"};
+    scenario_file_button = new QPushButton{"&Choose a scenario file"};
     controls_layout->addWidget(scenario_file_button);
-    // connect();
+    connect(scenario_file_button, &QPushButton::clicked, this, &ScenarioControls::setScenarioFile);
     QLabel *chosen_map_label = new QLabel{"Pick a scenario file first"};
     controls_layout->addWidget(chosen_map_label);
     setLayout(controls_layout);
 }
 
-void setScenarioFile() {
-    
+void ScenarioControls::setScenarioFile() {
+   QString ret = QFileDialog::getOpenFileName(this, 
+         tr("Choose a Scenario File"), ".", tr("Scenario Files (*.map.scen)"));
+  if (!ret.isEmpty()) {
+    QString new_text;
+    if (int index=ret.lastIndexOf("/")) {
+      new_text = ret.sliced(index + 1);
+    } else {
+      new_text=ret;
+    }
+    scenario_file_button->setText(new_text);
+  }
 }
--- src/ui/scenario_controls.h
+++ src/ui/scenario_controls.h
@@ -1,11 +1,17 @@
 #pragma once
-
 #include <QBoxLayout>
 #include <QGroupBox>
 #include <QPushButton>
 #include <QLabel>
+#include <QFileDialog>
+
+
 class ScenarioControls : public QGroupBox {
     // ScenarioService scenario_service;
 public:
     ScenarioControls();
+private slots:
+    void setScenarioFile();
+private:
+    QPushButton *scenario_file_button;
 };
